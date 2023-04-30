6883c2c0d1840c60dbb48ecb0da5145232b79117
Ayanaklld <124424359+Ayanaklld@users.noreply.github.com>
2023-04-30 13:06:34 +0600

    Update Main.java

--- src/BookMover/Main.java
+++ src/BookMover/Main.java
@@ -2,26 +2,30 @@ package BookMover;
 
 public class Main {
     public static void main(String[] args) {
-//        Book book = new Book("The Lord of the Rings", Status.AVAILABLE);
-//        BookMover fromAvailableStatusMover = new FromAvailableStatusMover();
-//        fromAvailableStatusMover.moveToStatus(book, Status.OVERDUED);
-//        System.out.println(book.getStatus());
+        Book b1 = new Book("Java");
+        BookMover fromAvailableStatusMover = new FromAvailableStatusMover();
+        fromAvailableStatusMover.moveToStatus(b1, Status.ARCHIVED);
+        System.out.println(b1.getStatus());
+        System.out.println(" ");
 
-        Book book = new Book("1984");
+        BookMover fromArchivedStatusMover = new FromArchievedStatusMover();
+        fromArchivedStatusMover.moveToStatus(b1, Status.AVAILABLE);
+        System.out.println(b1.getStatus());
+        System.out.println(" ");
+
+        BookMover fromAvailableStatusMover1 = new FromAvailableStatusMover();
+        fromAvailableStatusMover1.moveToStatus(b1, Status.BORROWED);
+        System.out.println(b1.getStatus());
+        System.out.println(" ");
+
         BookMover fromBorrowedStatusMover = new FromBorrowedStatusMover();
-        fromBorrowedStatusMover.moveToStatus(book, Status.OVERDUED);
-        System.out.println(book.getStatus());
+        fromBorrowedStatusMover.moveToStatus(b1, Status.OVERDUED);
+        System.out.println(b1.getStatus());
+        System.out.println(" ");
 
-//          Book book = new Book("Atomic habits");
-//          BookMover fromArchivedStatusMover = new FromArchievedStatusMover();
-//          fromArchivedStatusMover.moveToStatus(book, Status.BORROWED);
-//          System.out.println(book.getStatus());
-
-//            Book book = new Book("Sapiens");
-//            BookMover fromOverduedStatusMover = new FromOverduedStatusMover();
-//            fromOverduedStatusMover.moveToStatus(book, Status.AVAILABLE);
-//            System.out.println(book.getStatus());
-
-
+        BookMover fromOverduedStatusMover = new FromOverduedStatusMover();
+        fromOverduedStatusMover.moveToStatus(b1, Status.AVAILABLE);
+        System.out.println(b1.getStatus());
+        System.out.println(" ");
     }
 }
